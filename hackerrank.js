f92c273fa02ad72164398a1d77845e7b022bdc24
asharali95 <ashar.alia1999@gmail.com>
2022-04-26 22:17:54 +0500

    minmax algo done

--- hackerrank.js
+++ hackerrank.js
@@ -89,3 +89,28 @@ function compareTriplets(a, b) {
 //   }
 //   console.log(stair);
 // }
+
+// ----------------------------------------------------------
+// QUESTION 04 - MinMax sum
+
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+// Example:
+// arr = [1,3,5,7,9]
+// The minimum sum is 1+3+5+7 = 16 and the maximum sum is 3+5+7+9 = 24
+// the function prints
+// 16 24
+
+// function miniMaxSum(arr) {
+//   let allSums = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let temp = arr[i];
+//     let sum = 0;
+//     for (let j = 0; j < arr.length; j++) {
+//       if (temp === arr[j] && j === i) continue;
+//       sum += arr[j];
+//     }
+//     allSums.push(sum);
+//   }
+//   console.log(Math.min(...allSums) + " " + Math.max(...allSums));
+// }
